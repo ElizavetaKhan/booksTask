3b78eb61e77d04096f4639fe5be6fe68e9ac75bf
llizz.kh <lizakhan2001@mail.ru>
2025-10-29 16:23:40 +0300

    add database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,59 @@
+INSERT INTO books (title, author, description, publish_year) VALUES
+('Преступление и наказание', 'Ф.М. Достоевский', 'Роман о нравственных терзаниях студента Раскольникова', 1866);
+
+INSERT INTO books (title, author, description, publish_year) VALUES
+('Мастер и Маргарита', 'М.А. Булгаков', 'Роман о борьбе добра и зла в Москве', 1967);
+
+INSERT INTO books (title, author, description, publish_year) VALUES
+('Война и мир', 'Л.Н. Толстой', 'Исторический роман о судьбах людей во время Наполеоновских войн', 1869);
+
+INSERT INTO books (title, author, description, publish_year) VALUES
+('Анна Каренина', 'Л.Н. Толстой', 'Роман о трагической любви и семейных конфликтах', 1877);
+
+INSERT INTO books (title, author, description, publish_year) VALUES
+('Идиот', 'Ф.М. Достоевский', 'Роман о чистоте и доброте в жестоком мире', 1869);
+
+INSERT INTO books (title, author, description, publish_year) VALUES
+('Братья Карамазовы', 'Ф.М. Достоевский', 'Философский роман о вере, морали и семье', 1880);
+
+INSERT INTO books (title, author, description, publish_year) VALUES
+('Доктор Живаго', 'Б. Пастернак', 'Роман о судьбах людей во время революции и войны', 1957);
+
+INSERT INTO books (title, author, description, publish_year) VALUES
+('Обломов', 'И.А. Гончаров', 'Роман о праздности, лени и судьбе человека', 1859);
+
+INSERT INTO books (title, author, description, publish_year) VALUES
+('Евгений Онегин', 'А.С. Пушкин', 'Роман в стихах о любви и судьбе Онегина', 1833);
+
+INSERT INTO books (title, author, description, publish_year) VALUES
+('Мертвые души', 'Н.В. Гоголь', 'Сатирический роман о нравственных пороках общества', 1842);
+
+INSERT INTO books (title, author, description, publish_year) VALUES
+('Капитанская дочка', 'А.С. Пушкин', 'Исторический роман о Пугачевском восстании', 1836);
+
+INSERT INTO books (title, author, description, publish_year) VALUES
+('Тихий Дон', 'М.А. Шолохов', 'Роман о казачьей жизни и революции', 1940);
+
+INSERT INTO books (title, author, description, publish_year) VALUES
+('Собачье сердце', 'М.А. Булгаков', 'Сатира на эксперименты над человеком и обществом', 1925);
+
+INSERT INTO books (title, author, description, publish_year) VALUES
+('Белая гвардия', 'М.А. Булгаков', 'Роман о судьбе семьи в годы Гражданской войны', 1925);
+
+INSERT INTO books (title, author, description, publish_year) VALUES
+('Пиковая дама', 'А.С. Пушкин', 'Повесть о страсти к азарту и судьбе', 1834);
+
+INSERT INTO books (title, author, description, publish_year) VALUES
+('Отцы и дети', 'И.С. Тургенев', 'Роман о конфликте поколений и нигилизме', 1862);
+
+INSERT INTO books (title, author, description, publish_year) VALUES
+('Лолита', 'В. Набоков', 'Роман о запретной любви и сложных моральных выборах', 1955);
+
+INSERT INTO books (title, author, description, publish_year) VALUES
+('Приключения Шерлока Холмса', 'А. Конан Дойл', 'Сборник детективных историй о знаменитом сыщике', 1892);
+
+INSERT INTO books (title, author, description, publish_year) VALUES
+('Гордость и предубеждение', 'Дж. Остин', 'Роман о любви, семье и социальных нравах', 1813);
+
+INSERT INTO books (title, author, description, publish_year) VALUES
+('Моби Дик', 'Г. Мелвилл', 'Роман о китобое и его одержимости белым китом', 1851);
